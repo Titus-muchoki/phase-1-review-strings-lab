7c40d26d78a133ffc3d026579b0edde19bb3caae
kajela <titoyut56@gmail.com>
2023-05-03 08:13:33 -0400

    add the initial files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Tito Muchoki';
 const welcomeMessage = `'Welcome to Flatbook, '  ${currentUser}!`;
-const excitedWelcomeMessage =  'WELCOME TO FLATBOOK, '
+const excitedWelcomeMessage = ` 'WELCOME TO FLATBOOK, ' ${currentUser.toUpperCase()}!`
