b4f2f0193467ca2b429d401f724dcef39d7d4fdd
kajela <titoyut56@gmail.com>
2023-05-03 08:17:34 -0400

    add the initial files

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Tito Muchoki';
 const welcomeMessage = `'Welcome to Flatbook, '  ${currentUser}!`;
 const excitedWelcomeMessage = ` 'WELCOME TO FLATBOOK, ' ${currentUser.toUpperCase()}!`
+const shortGreeting =  'Welcome, '
